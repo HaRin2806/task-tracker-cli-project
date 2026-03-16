4a0b92b8c887b3d86b0eddcc8c747e37d70f3e09
HaRin2806 <2151062809@e.tlu.edu>
2026-03-16 22:01:37 +0700

    initial commit

--- TaskManager.cs
+++ TaskManager.cs
@@ -9,113 +9,113 @@ public class TaskManager
     private const string FileName = "task.json";
 
     private List<TaskItem> LoadTasks()
-{
-    if (!File.Exists(FileName))
     {
-        File.WriteAllText(FileName, "[]");
-    }
-    string json = File.ReadAllText(FileName);
-
-    return JsonSerializer.Deserialize<List<TaskItem>>(json);
-}
-
-private void SaveTasks(List<TaskItem> tasks)
-{
-    string json = JsonSerializer.Serialize(tasks, new JsonSerializerOptions
-    {
-        WriteIndented = true
-    });
-
-    File.WriteAllText(FileName, json);
-}
-public void AddTask(string description)
-{
-    var tasks = LoadTasks();
-
-    int newId = tasks.Count == 0 ? 1 : tasks[tasks.Count - 1].Id + 1;
-
-    var task = new TaskItem
-    {
-        Id = newId,
-        Description = description,
-        Status = "todo",
-        CreatedAt = DateTime.Now,
-        UpdatedAt = DateTime.Now
-    };
-
-    tasks.Add(task);
-
-    SaveTasks(tasks);
-
-    Console.WriteLine($"Task added successfully (ID: {newId})");
-}
-public void UpdateTask(int id, string description)
-{
-    var tasks = LoadTasks();
-
-    var task = tasks.Find(t => t.Id == id);
-
-    if (task == null)
-    {
-        Console.WriteLine("Task not found");
-        return;
-    }
-
-    task.Description = description;
-    task.UpdatedAt = DateTime.Now;
-
-    SaveTasks(tasks);
-
-    Console.WriteLine("Task updated");
-}
-public void DeleteTask(int id)
-{
-    var tasks = LoadTasks();
-
-    var task = tasks.Find(t => t.Id == id);
-
-    if (task == null)
-    {
-        Console.WriteLine("Task not found");
-        return;
-    }
-
-    tasks.Remove(task);
-
-    SaveTasks(tasks);
-
-    Console.WriteLine("Task deleted");
-}
-public void MarkStatus(int id, string status)
-{
-    var tasks = LoadTasks();
-
-    var task = tasks.Find(t => t.Id == id);
-
-    if (task == null)
-    {
-        Console.WriteLine("Task not found");
-        return;
-    }
-
-    task.Status = status;
-    task.UpdatedAt = DateTime.Now;
-
-    SaveTasks(tasks);
-
-    Console.WriteLine($"Task marked as {status}");
-}
-public void ListTasks(string status = null)
-{
-    var tasks = LoadTasks();
-
-    foreach (var task in tasks)
-    {
-        if (status == null || task.Status == status)
+        if (!File.Exists(FileName))
         {
-            Console.WriteLine($"{task.Id} | {task.Description} | {task.Status}");
+            File.WriteAllText(FileName, "[]");
+        }
+        string json = File.ReadAllText(FileName);
+
+        return JsonSerializer.Deserialize<List<TaskItem>>(json);
+    }
+
+    private void SaveTasks(List<TaskItem> tasks)
+    {
+        string json = JsonSerializer.Serialize(tasks, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+
+        File.WriteAllText(FileName, json);
+    }
+    public void AddTask(string description)
+    {
+        var tasks = LoadTasks();
+
+        int newId = tasks.Count == 0 ? 1 : tasks[tasks.Count - 1].Id + 1;
+
+        var task = new TaskItem
+        {
+            Id = newId,
+            Description = description,
+            Status = "todo",
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now
+        };
+
+        tasks.Add(task);
+
+        SaveTasks(tasks);
+
+        Console.WriteLine($"Task added successfully (ID: {newId})");
+    }
+    public void UpdateTask(int id, string description)
+    {
+        var tasks = LoadTasks();
+
+        var task = tasks.Find(t => t.Id == id);
+
+        if (task == null)
+        {
+            Console.WriteLine("Task not found");
+            return;
+        }
+
+        task.Description = description;
+        task.UpdatedAt = DateTime.Now;
+
+        SaveTasks(tasks);
+
+        Console.WriteLine("Task updated");
+    }
+    public void DeleteTask(int id)
+    {
+        var tasks = LoadTasks();
+
+        var task = tasks.Find(t => t.Id == id);
+
+        if (task == null)
+        {
+            Console.WriteLine("Task not found");
+            return;
+        }
+
+        tasks.Remove(task);
+
+        SaveTasks(tasks);
+
+        Console.WriteLine("Task deleted");
+    }
+    public void MarkStatus(int id, string status)
+    {
+        var tasks = LoadTasks();
+
+        var task = tasks.Find(t => t.Id == id);
+
+        if (task == null)
+        {
+            Console.WriteLine("Task not found");
+            return;
+        }
+
+        task.Status = status;
+        task.UpdatedAt = DateTime.Now;
+
+        SaveTasks(tasks);
+
+        Console.WriteLine($"Task marked as {status}");
+    }
+    public void ListTasks(string status = null)
+    {
+        var tasks = LoadTasks();
+
+        foreach (var task in tasks)
+        {
+            if (status == null || task.Status == status)
+            {
+                Console.WriteLine($"{task.Id} | {task.Description} | {task.Status}");
+            }
         }
     }
-}
 }
 
--- obj/Debug/net10.0/TaskTrackerCLI.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskTrackerCLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTrackerCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af609773f9770b9f185b54a89035dc7c82ecbdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTrackerCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTrackerCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
