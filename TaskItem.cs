b70efa40c7d31f046420c2e68dee0adc40a4a865
HaRin2806 <2151062809@e.tlu.edu>
2026-03-16 21:40:32 +0700

    Initial commit -  Task Tracker CLI in C#

---
+++ TaskItem.cs
@@ -0,0 +1,9 @@
+using System;
+public class TaskItem
+{
+    public int Id {get; set;}
+    public string Description {get; set;}
+    public string Status {get; set;}
+    public DateTime CreatedAt {get; set;}
+    public DateTime UpdatedAt {get; set;}
+}
